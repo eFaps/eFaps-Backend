38c4e3f80cf6753f3f31721a5f5cdfc690cab52c
Jan Moxter <jan@moxter.net>
2023-07-27 16:24:49 -0500

    Sever url via config

--- src/main/java/org/efaps/Main.java
+++ src/main/java/org/efaps/Main.java
@@ -3,22 +3,26 @@ package org.efaps;
 import java.io.IOException;
 import java.net.URI;
 
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import jakarta.ws.rs.core.UriBuilder;
+public class Main
+{
 
-
-public class Main {
     protected static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws InterruptedException, IOException {
-        final URI baseUri = UriBuilder.fromUri("http://localhost/").port(8080).build();
+    public static void main(String[] args)
+        throws InterruptedException, IOException
+    {
+        final ResourceConfig restConfig = new RestConfig();
 
-        final ResourceConfig config = new RestConfig();
-        final var server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config);
+        final var config = ConfigProvider.getConfig();
+        final URI baseUri = config.getValue("sever.url", URI.class);
+
+        final var server = GrizzlyHttpServerFactory.createHttpServer(baseUri, restConfig);
         server.start();
 
         System.out.println();
