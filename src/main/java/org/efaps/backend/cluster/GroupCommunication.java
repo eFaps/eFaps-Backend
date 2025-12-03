8eeb695571159242b410d9cba4e0441fd552de12
Jan Moxter <jan.moxter@moxter.net>
2025-12-02 19:56:30 -0500

    Logging

--- src/main/java/org/efaps/backend/cluster/GroupCommunication.java
+++ src/main/java/org/efaps/backend/cluster/GroupCommunication.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2003 - 2024 The eFaps Team (-)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.efaps.backend.cluster;
 
 import org.eclipse.microprofile.config.Config;
@@ -30,13 +45,13 @@ public class GroupCommunication
                     @Override
                     public void receive(final Message msg)
                     {
-                        LOG.info("received message: {}", msg);
+                        LOG.info("received JGroups message: {}", msg);
                     }
 
                     @Override
                     public void viewAccepted(final View view)
                     {
-                        LOG.info("received view: {}", view);
+                        LOG.info("received JGroups view: {}", view);
                     }
                 });
 
@@ -46,7 +61,6 @@ public class GroupCommunication
             } catch (final Exception e) {
                 LOG.error("Catched", e);
             }
-
         } else {
             LOG.debug("No JGroupsFile");
         }
