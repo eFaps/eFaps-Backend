748fc2011e65583eba76bf440f01bce59ac3e229
Jan Moxter <jan.moxter@moxter.net>
2026-02-19 21:16:17 -0500

    Add ExecutorService to CheckoutResource

--- src/main/java/org/efaps/backend/resources/CheckoutResource.java
+++ src/main/java/org/efaps/backend/resources/CheckoutResource.java
@@ -22,7 +22,11 @@ import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.apache.commons.lang3.time.StopWatch;
 import org.apache.tika.Tika;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.efaps.db.Checkout;
@@ -32,16 +36,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotAcceptableException;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.container.AsyncResponse;
+import jakarta.ws.rs.container.CompletionCallback;
 import jakarta.ws.rs.container.Suspended;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.ResponseBuilder;
 import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.core.StreamingOutput;
+import jakarta.ws.rs.core.UriInfo;
 
 @Path("checkout")
 public class CheckoutResource
@@ -50,13 +58,23 @@ public class CheckoutResource
     private static final Logger LOG = LoggerFactory.getLogger(CheckoutResource.class);
     private static URI TMPURI;
     private static boolean CHECKED;
+    private static ExecutorService EXECSERVICE = Executors.newCachedThreadPool(BasicThreadFactory.builder()
+                    .namingPattern("CheckoutThread-%d")
+                    .build());
 
     @GET
     @Produces({ MediaType.APPLICATION_OCTET_STREAM, MediaType.APPLICATION_JSON })
-    public void checkout(@QueryParam("oid") final String oid,
-                         @Suspended final AsyncResponse asyncResponse)
+    public void checkout(@Suspended final AsyncResponse asyncResponse,
+                         @Context UriInfo uriInfo,
+                         @QueryParam("oid") final String oid)
         throws IOException, EFapsException
     {
+        if (oid == null) {
+            throw new NotAcceptableException();
+        }
+        final var stopWatch = new StopWatch();
+        stopWatch.start();
+
         final Instance instance = Instance.get(oid);
         final Checkout checkout = new Checkout(instance);
         final File file;
@@ -71,8 +89,19 @@ public class CheckoutResource
         } catch (final EFapsException e) {
             asyncResponse.resume(Response.ok(Status.NOT_FOUND).build());
         }
+
+        asyncResponse.register((CompletionCallback) throwable -> {
+            stopWatch.stop();
+            LOG.info("Checkout with {} finished in {}", uriInfo.getQueryParameters(true),
+                            stopWatch.formatTime());
+            if (file.exists()) {
+                file.delete();
+            }
+        });
+
         final String mimeType = new Tika().detect(file);
-        new Thread(() -> {
+
+        EXECSERVICE.execute(() -> {
             final StreamingOutput fileStream = output -> {
                 try (var in = new FileInputStream(file)) {
                     final byte[] buffer = new byte[8192];
@@ -88,7 +117,7 @@ public class CheckoutResource
             response.header("Content-Disposition", "attachment; filename=\"" + checkout.getFileName() + "\"");
             response.header("Content-Length", checkout.getFileLength());
             asyncResponse.resume(Response.ok(fileStream).build());
-        }).start();
+        });
     }
 
     private URI getTmpURI()
